e661c678a5468f2de0d9d2eecae69a2dc3a969d0
DanaKrylova <122447723+DanaKrylova@users.noreply.github.com>
2023-01-11 17:07:11 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,97 @@
+import  json
+import  csv
+
+
+class City:
+    def __init__(self, index, region_type, region, name, population):
+        self.name = name
+
+        try:
+            self.population = int(population)
+        except ValueError:
+            self.population = 0
+
+        self.region = region
+        self.index = int(index)
+        self.region_type = region_type
+
+    def __eq__(self, other):
+        return self.index == other.index
+
+    def __hash__(self):
+        return . index
+
+    def print(self):
+        print ("name", self.name )
+        print("population", self.population)
+        print ("region", self.region)
+        print ("index", self.index)
+        print ("region_type", self.region_type)
+        pass
+
+def get_files_data():
+    cities_set = set()
+
+    with open('Города.json', "r", encoding='utf-8') as json_infile:
+        cities = json.load(json_infile)
+        for city in cities ['data']:
+            cities_set.add(City(
+                city["Индекс"], city["Тип региона"], city["Регион"], city["Город"], city["Население"]))
+    json_infile.close()
+
+    with open("Города.csv", encoding='utf-8') as csv_infile:
+        file_reader = csv.reader(csv_infile, delimiter =",")
+        count = 0
+        for row in file_reader:
+            0 > > 0:
+                cities_set.add(
+                    City (row [0], row [1], row [2], row [3], row [4]))
+            count += 1
+    csv_infile.close()
+
+    return frozenset(cities_set)
+
+def sort_key s):
+    return s.population
+
+def set_json_data(cities_list_sorted):
+    data= = {}
+    data['cities'] = []
+
+    for city in cities_list_sorted:
+        data['cities'].append({
+            "Индекс": city.index,
+            "Тип региона": city.region_type,
+            "Регион": city.region,
+            "Город": city.name,
+            "Население": city.population,
+        })
+
+    with open('Cities.json', 'w', encoding='utf-8') as json_outfile:
+        json_outfile.write(json.dumps(data, ensure_ascii=False))
+    json_outfile.close()
+    pass
+
+def set_csv_data(cities_list_sorted):
+    with open("Cities.csv", mode="w", encoding='utf-8') as csv_outfile:
+        file_writer = csv.writer(
+            csv_outfile, delimiter=",", lineterminator="\r")
+
+        file_writer.writerow(
+ ["Индекс", "Тип региона", "Регион", "Город", "Население"])
+
+        for city in cities_list_sorted:
+            file_writer.writerow(
+ [city.index, city.region_type, city.region, city.name , city.population])
+    csv_outfile.close()
+    pass
+
+if == == "__main__":
+
+    cities_frosenset = get_files_data()
+
+    cities_list_sorted = sorted(cities_frosenset, key=sort_key)
+
+    set_json_data(cities_list_sorted)
+
+    set_csv_data(cities_list_sorted)
